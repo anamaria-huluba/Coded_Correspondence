37f5623ed982ef10749efaa7097dff4937a8975a
Anamaria Huluba <82522372+anamaria-huluba@users.noreply.github.com>
2022-01-28 15:08:01 +0000

    Update Strings - Copeland's_Corporate_Company.py

--- Strings - Copeland's_Corporate_Company.py
+++ Strings - Copeland's_Corporate_Company.py
@@ -1,14 +1,14 @@
-# 1. Copeland’s Corporate Company has finalized what they want their username and temporary password 
-# creation to be and have enlisted your help, once again, to build the function to generate them. In this 
-# exercise, you will create two functions, username_generator and password_generator.
+# LEARN PYTHON 3
+Strings - Copeland's_Corporate_Company
 
-# Let’s start with username_generator. 
+1. Copeland’s Corporate Company has finalized what they want their username and temporary password creation to be and have enlisted your help, once again, to build the function 
+to generate them. In this exercise, you will create two functions, username_generator and password_generator.
 
-# Create a function called username_generator take two inputs, first_name and last_name and returns a 
-# username. The username should be a slice of the first three letters of their first name and the first 
-# four letters of their last name. 
-# If their first name is less than three letters or their last name is less than four letters it should 
-# use their entire names.
+Let’s start with username_generator. 
+
+Create a function called username_generator take two inputs, first_name and last_name and returns a username. The username should be a slice of the first three letters of 
+their first name and the first four letters of their last name. If their first name is less than three letters or their last name is less than four letters it should use their
+entire names.
 
 def username_generator(first_name, last_name):
     if len(first_name) < 3:
@@ -21,22 +21,18 @@ def username_generator(first_name, last_name):
         user_name += last_name[0:4]
     return user_name
 
-#For example, if the employee’s name is Anamaria Huluba the function should generate the username AnaHulu.
+For example, if the employee’s name is Anamaria Huluba the function should generate the username AnaHulu.
 
 print(username_generator("Anamaria", "Huluba"))
 
-# 2. Now for the temporary password, they want the function to take the input user name and shift all of 
-# the letters by one to the right, so the last letter of the username ends up as the first letter and so 
-# forth. 
+2. Now for the temporary password, they want the function to take the input user name and shift all of the letters by one to the right, so the last letter of the username 
+ends up as the first letter and so forth. 
 
-# Start by defining the function password_generator so that it takes one input, username and in it define 
-# an empty string named password.
+Start by defining the function password_generator so that it takes one input, username and in it define an empty string named password.
 
-# Inside password_generator create a for loop that iterates through the indices username by going from 0 
-# to len(username).
+Inside password_generator create a for loop that iterates through the indices username by going from 0 to len(username).
 
-# To shift the letters right, at each letter the for loop should add the previous letter to the string 
-# password.
+To shift the letters right, at each letter the for loop should add the previous letter to the string password.
 
 def password_generator(user_name):
     password = ""
