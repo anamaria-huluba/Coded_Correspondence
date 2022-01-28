1d162b1fcf42987486911157c3fb6b0d4c425fa9
Anamaria Huluba <82522372+anamaria-huluba@users.noreply.github.com>
2022-01-28 14:58:54 +0000

    Update Files - Hacking_The_Fender.py

--- Files - Hacking_The_Fender.py
+++ Files - Hacking_The_Fender.py
@@ -1,4 +1,5 @@
-#Hacking The Fender
+# LEARN PYTHON 3
+# Hacking The Fender
 
 # The Fender, a notorious computer hacker and general villain of the people, has compromised several 
 # top-secret passwords including your own. Your mission, should you choose to accept it, is threefold. 
